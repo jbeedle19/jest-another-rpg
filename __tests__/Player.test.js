5ce4015add4a6246d8d947526f8f75339afc4326
jbeedle19 <josh.beedle@gmail.com>
2020-12-22 10:46:18 -0500

    create failed tests for getInventory and getStats

--- __tests__/Player.test.js
+++ __tests__/Player.test.js
@@ -16,3 +16,22 @@ test('creates a player object', () => {
         expect.arrayContaining([expect.any(Object)])
     );
 });
+
+test("gets player's stats as an object", () => {
+    const player = new Player('Dave');
+
+    expect(player.getStats()).toHavePoperty('potions');
+    expect(player.getStats()).toHavePoperty('health');
+    expect(player.getStats()).toHavePoperty('strength');
+    expect(player.getStats()).toHavePoperty('agility');
+});
+
+test('gets inventory from player or returns false', () => {
+    const player = new Player('Dave');
+
+    expect(player.getInventory()).toEqual(expect.any(Array));
+
+    player.inventory = [];
+
+    expect(player.getInventory()).toEqual(false);
+});
