84950ade667f0b358318c0f1fa670025322eaf22
jbeedle19 <josh.beedle@gmail.com>
2021-01-05 09:40:25 -0500

    tested and created isAlive and reduceHealth

--- __tests__/Player.test.js
+++ __tests__/Player.test.js
@@ -40,4 +40,29 @@ test("gets player's health value", () => {
     const player = new Player('Dave');
 
     expect(player.getHealth()).toEqual(expect.stringContaining(player.health.toString()));
-})
+});
+
+test("checks if player is alive or not", () => {
+    const player = new Player('Dave');
+
+    expect(player.isAlive()).toBeTruthy();
+
+    player.health = 0;
+
+    expect(player.isAlive()).toBeFalsy();
+});
+
+test("subtracts from player's health", () => {
+    // Create a new 'Player' instance in every test to avoid unintended consequences
+    // This helps us test things in isolation
+    const player = new Player('Dave');
+    const oldHealth = player.health;
+
+    player.reduceHealth(5);
+
+    expect(player.health).toBe(oldHealth - 5);
+
+    player.reduceHealth(99999);
+
+    expect(player.health).toBe(0);
+});
